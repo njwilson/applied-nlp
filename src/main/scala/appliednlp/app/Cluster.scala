eb2f8bf85b0e281403b7abc9e06f09d8a1724aeb
Nick Wilson <nick@njwilson.net>
2013-02-19 11:14:11 -0600

    Implement basic CLI clustering support
    Adds support for CLI options filename, distance, k, and showCentroid

--- src/main/scala/appliednlp/app/Cluster.scala
+++ src/main/scala/appliednlp/app/Cluster.scala
@@ -20,14 +20,26 @@ object Cluster {
   def main(args: Array[String]) {
     // Parse and get the command-line options
     val opts = ClusterOpts(args)
-    
+
     // This tells the k-means algorithm how much debugging information to show you
-    // while you run the algorithm. 
+    // while you run the algorithm.
     val logLevel = if (opts.verbose()) Level.DEBUG else Level.INFO
     Logger.getRootLogger.setLevel(logLevel)
-    
-    // Your code starts here. You'll use and extend it during every problem.
 
+    // Load the data to cluster
+    val pointCreator = DirectCreator
+    val pointData = pointCreator(opts.filename())
+
+    // Run Kmeans
+    val points = pointData.map(_._3).toIndexedSeq
+    val distanceFunction = DistanceFunction(opts.distance())
+    val kmeans = new Kmeans(points, distanceFunction, fixedSeedForRandom=true)
+    val (dispersion, centroids) = kmeans.run(opts.k())
+
+    // Print the coordinates of each centroid
+    if (opts.showCentroids()) {
+      centroids.foreach(println)
+    }
 
   }
 
@@ -41,7 +53,7 @@ object Cluster {
 object ClusterOpts {
 
   import org.rogach.scallop._
-  
+
   def apply(args: Array[String]) = new ScallopConf(args) {
     banner("""
 Cluster application using nak.cluster.Kmeans
@@ -54,13 +66,13 @@ The transform option is one ident, zscore, and pca.
  - pca: scale the points with z-scores, then run PCA and use (as transformed dimensions) only the principle components that explain 95% of the variance
 
 For usage see below:
-	     """)
+            """)
 
     val featureTypes = Set("standard","schools","countries","fed-simple","fed-full")
     val transformers = Set("i","ident","z","zscore","p","pca")
     val distanceFunctions = Set("c","cosine","e","euclidean","m","manhattan")
     val transform = opt[String]("transform", default=Some("ident"), validate = transformers, descr = "The transformation to use. Possible values: " + transformers.toSeq.sorted.mkString(",") )
-    val distance = opt[String]("dist", default=Some("cosine"), validate = distanceFunctions, descr = "The distance function to use. Possible values: " + distanceFunctions.toSeq.sorted.mkString(",") )      
+    val distance = opt[String]("dist", default=Some("cosine"), validate = distanceFunctions, descr = "The distance function to use. Possible values: " + distanceFunctions.toSeq.sorted.mkString(",") )
     val features = opt[String]("features", default=Some("standard"), validate = featureTypes, descr = "The type of features to extract. Possible values: " + distanceFunctions.toSeq.sorted.mkString(",") )
     val k = opt[Int]("num-clusters",short='k', required=true, validate = (0<), descr="The number of clusters to find.")
     val help = opt[Boolean]("help", noshort = true, descr = "Show this message")
